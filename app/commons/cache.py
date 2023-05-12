2cc9a8947d0500b890cdc427236e030e39d357d5
Varun Singhal <varunsinghal15@gmail.com>
2023-05-12 12:19:16 +0530

    added cache adaptor to adapt new implementations #9 (#10)

--- app/commons/cache.py
+++ app/commons/cache.py
@@ -1,5 +1,6 @@
 import functools
 import logging
+from abc import ABC, abstractmethod
 from typing import Optional
 
 from cachelib import SimpleCache
@@ -7,40 +8,66 @@ from cachelib import SimpleCache
 logger = logging.getLogger(__name__)
 
 
-class Cache:
+class ICache(ABC):
     """
-    Cache class used to interact with the rest of the code.
-    The strategy can be replaced with other implementations
-    without impacting the cache behavior.
+    Cache interface used to define the template for the rest
+    of the code. The adaptor can be used to adapt to other
+    implementations without impacting the cache behavior.
     """
 
-    def __init__(self, strategy) -> None:
-        self.strategy = strategy
+    @abstractmethod
+    def hashkey(self, func, *args):
+        pass
+
+    @abstractmethod
+    def has(self, key):
+        pass
+
+    @abstractmethod
+    def get(self, key):
+        pass
+
+    @abstractmethod
+    def set(self, key, value):
+        pass
+
+    @abstractmethod
+    def delete(self, key):
+        pass
+
+    @abstractmethod
+    def clear(self):
+        pass
+
+
+class CacheAdaptor(ICache):
+    def __init__(self, adaptee) -> None:
+        self.adaptee = adaptee
 
     def hashkey(self, func, *args):
         args_key = ".".join(map(lambda x: str(x), args))
         return func.__name__ + "." + args_key
 
     def has(self, key):
-        return self.strategy.has(key)
+        return self.adaptee.has(key)
 
     def get(self, key):
-        return self.strategy.get(key)
+        return self.adaptee.get(key)
 
     def set(self, key, value):
-        return self.strategy.set(key, value)
+        return self.adaptee.set(key, value)
 
     def delete(self, key):
-        return self.strategy.delete(key)
+        return self.adaptee.delete(key)
 
     def clear(self):
-        return self.strategy.clear()
+        return self.adaptee.clear()
 
 
-_cache = Cache(strategy=SimpleCache(threshold=300, default_timeout=20))
+_cache = CacheAdaptor(adaptee=SimpleCache(threshold=300, default_timeout=20))
 
 
-def ttl_cache(func, cache_cls: Optional[Cache] = _cache):
+def ttl_cache(func, cache_cls: Optional[ICache] = _cache):
     """
     Cache the output of the function provided in `func`.
     Builds the key using the function name and arguments provided.
