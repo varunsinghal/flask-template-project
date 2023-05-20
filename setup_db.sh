3b8ad8aec0c8a7cec429264e3769064d27a93316
varunsinghal <varunsinghal15@gmail.com>
2023-05-20 15:08:22 +0530

    updated readme with project description and notes #11

--- setup_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose up -d database
-sleep 5s
-docker-compose exec database bash -c "./load_db.sh"
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose build && docker-compose run --rm flask bash -c "python3 -m unittest discover tests"
